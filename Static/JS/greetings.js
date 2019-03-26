cd58a13fd4b625caf7e0a071092b6fb3c56ac2fd
esterllayne <44364097+esterllayne@users.noreply.github.com>
2019-03-26 17:22:58 +0000

    trying to merge into master

---
+++ Static/JS/greetings.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+//GOOD EVENING SCRIPT-->
+
+    // Get the modal
+    var modal = document.getElementById('myModalEvening');
+    
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtnEvening");
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+
+
+
+
+
+
+
+//GOOD NIGHT SCRIPT-->
+    
+        // Get the modal
+        var modal = document.getElementById('myModalNight');
+        
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtnNight");
+        
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+          modal.style.display = "block";
+        }
+        
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+
