09b7f83718450e4cb6d289503615e3849869e1a8
Monika Novackova <monika.novac@gmail.com>
2019-04-02 03:01:01 +0100

    center

--- speakSL.py
+++ speakSL.py
@@ -87,8 +87,7 @@ def returnResults():
     form_data=request.form
     correctLetters=[form_data["Q1"],form_data["Q2"],form_data["Q3"],form_data["Q4"],form_data["Q5"],form_data["Q6"]]
     print(correctLetters)
-    userAnswers=[form_data["id1"], form_data["id2"], form_data["id3"], form_data["id4"]
-   form_data["id5"],form_data["id6"] ]
+    userAnswers=[form_data["id1"], form_data["id2"], form_data["id3"], form_data["id4"],form_data["id5"],form_data["id6"] ]
     your_score=evaluate_score(userAnswers,correctLetters)[0]
     your_scorePerc=evaluate_score(userAnswers,correctLetters)[1]
     return render_template("resultsQuiz.html",your_score=your_score,your_scorePerc=your_scorePerc)
