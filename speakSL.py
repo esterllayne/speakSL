f1bea81cb6295775f156852446b96487f6ed4a7d
Monika Novackova <monika.novac@gmail.com>
2019-04-02 17:57:59 +0100

    sad

--- speakSL.py
+++ speakSL.py
@@ -14,6 +14,19 @@ alphabetD={"a": "https://i.imgur.com/k01IMQh.jpg", "b":"https://i.imgur.com/i64U
 "v":"https://i.imgur.com/pGiMgvM.jpg","w":"https://i.imgur.com/mJzLDLp.jpg","x":"https://i.imgur.com/DnIUoVk.jpg",
 "y":"https://i.imgur.com/6qEgu6D.jpg","z":"https://i.imgur.com/h6oWvhS.jpg"}
 
+greetingsD={"Hello": "https://media.giphy.com/media/3o7TKNKOfKlIhbD3gY/giphy.gif","Good Morning":"https://media.giphy.com/media/26FLchGgqamznV64E/giphy.gif",
+"Good Afternoon":"https://media.giphy.com/media/l4JzaRsX52k8glIFa/giphy.gif","Good Evening":"https://media.giphy.com/media/l4JzdrbDeU2lMMrde/giphy.gif",
+"Good Night":"https://media.giphy.com/media/l4Jz5WK4Uddr8KsSc/giphy.gif","Thank You!":"https://media.giphy.com/media/l0MYrlUnFtq25TQR2/giphy.gif",
+"You're welcome!":"https://media.giphy.com/media/3o7TKSRNcdPmcNmTGo/giphy.gif",
+"How Are You?":"https://media.giphy.com/media/3o7TKDw5NA17fKJVWU/giphy.gif",
+"I'm fine.":"https://media.giphy.com/media/l4Jzd71ci3msO66ac/giphy.gif",
+"What's your name?":"https://media.giphy.com/media/3o7TKDJBonanzESryE/giphy.gif",
+"My name is...":"https://media.giphy.com/media/3o7TKzkaMOHallCppe/giphy.gif",
+"Nice to meet you!":"https://media.giphy.com/media/1oHlX1mrGBF5xu1ks1/giphy.gif","Goodbye":"https://media.giphy.com/media/3o7TKzb3i29i86BPJm/giphy.gif"}
+
+
+
+
 def evaluate_score(ansUsr,ansCorrect):
     score=0
     for i in range(len(ansUsr)):
@@ -49,23 +62,37 @@ def get_small():
 def get_alphabet():
     return render_template("alphabet.html")
 
-
 @app.route("/quiz")
 def get_quiz():
-    questionLabels=['Q1','Q2','Q3','Q4','Q5','Q6']
-    PickedLettersDict=pick(alphabetD)
-    return (render_template("quiz.html",PickedLettersDict=PickedLettersDict,qLabels=questionLabels))
+    return render_template("quiz.html")
 
-@app.route("/QuizResults", methods=["POST"])
+@app.route("/quizG")
+def get_quizG():
+    questionLabels=['Q1','Q2','Q3','Q4','Q5','Q6']
+    questionIDs=['id1','id2','id3','id4','id5','id6']
+    PickedLettersDict=pick(greetingsD)
+    return (render_template("quizG.html",PickedLettersDict=PickedLettersDict,qLabels=questionLabels,qIDs=questionIDs))
+
+
+@app.route("/quizA")
+def get_quizA():
+    questionLabels=['Q1','Q2','Q3','Q4','Q5','Q6']
+    questionIDs=['id1','id2','id3','id4','id5','id6']
+    PickedLettersDict=pick(alphabetD)
+    return (render_template("quizA.html",PickedLettersDict=PickedLettersDict,qLabels=questionLabels,qIDs=questionIDs))
+
+
+@app.route("/resultsQuiz", methods=["POST"])
 def returnResults():
     form_data=request.form
     correctLetters=[form_data["Q1"],form_data["Q2"],form_data["Q3"],form_data["Q4"],form_data["Q5"],form_data["Q6"]]
     print(correctLetters)
-    userAnswers=[form_data[correctLetters[0]], form_data[correctLetters[1]], form_data[correctLetters[2]], form_data[correctLetters[3]],
-    form_data[correctLetters[4]], form_data[correctLetters[5]] ]
+    userAnswers=[form_data["id1"], form_data["id2"], form_data["id3"], form_data["id4"],form_data["id5"],form_data["id6"] ]
     your_score=evaluate_score(userAnswers,correctLetters)[0]
     your_scorePerc=evaluate_score(userAnswers,correctLetters)[1]
-    return render_template("ResultsQ.html",your_score=your_score,your_scorePerc=your_scorePerc)
+    return render_template("resultsQuiz.html",your_score=your_score,your_scorePerc=your_scorePerc)
+
+
 
 if "AppSL" == '__main__':
     app.run()
